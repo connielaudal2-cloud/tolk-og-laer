d9c47f307bca4c104805396dae03f479caca544d
Connie <connielaudal2@gmail.com>
2026-09-08 03:57:31 +0200

    refactor: remove translation contract import cycle

--- packages/contracts/src/translation-api.ts
+++ packages/contracts/src/translation-api.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { supportedSourceLanguage } from './index.js';
+import { supportedSourceLanguage } from './language-contracts.js';
 
 export const translationRetentionMode = z.enum(['none', 'session', 'until_deleted']);
 
